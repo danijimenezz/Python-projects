87b25c4cce61d3bab9ad5e093be8df94190a727f
danijimenezz <danijimenezcampos8@gmail.com>
2022-10-31 13:03:51 +0100

    Create input_output_ex.py
    Python exercises to practice input and outputs

---
+++ exercises/input_output_ex.py
@@ -0,0 +1,14 @@
+# Ex 1: Write a program to accept two numbers from the user and calculate multiplication
+def mult():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number to multiply: "))
+    product = num1 * num2
+    print("The result of multiplying " +str(num1)+ " and " +str(num2)+ " is: " + str(product))
+    
+    
+mult()
+
+# Ex 2: 
+
+
+    
