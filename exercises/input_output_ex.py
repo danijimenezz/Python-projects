e9eee3560c290717ff2120bc872fdc76dc899ef4
danijimenezz <danijimenezcampos8@gmail.com>
2022-10-31 20:33:38 +0100

    Update input_output_ex.py

--- exercises/input_output_ex.py
+++ exercises/input_output_ex.py
@@ -8,7 +8,38 @@ def mult():
     
 mult()
 
-# Ex 2: 
 
+# Ex 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+print('My', 'name', 'is', sep='**')
+
+
+# Ex 3: Convert Decimal number to octal using print() output formatting
+print(oct(int(input("Enter a decimal number: "))))
+
+
+# Ex 4: Display float number with 2 decimal places
+num = float(input("Enter a float number"))
+print('%.2f' % num)
+
+
+# Ex 5: Accept a list of n float numbers as an input from the user
+
+def createList(len):
+    floatList = []
+    for i in range(len):
+        floatList.append(float(input("Enter number at index " +str(i)+ ": "))) 
+    print(floatList)
+    
+    
+len = int(input("Enter the length of your list of float numbers: "))
+createList(len)
+
+
+# Ex 6: Write all content of a given file into a new file by skipping line number 5
+    
+    
+    
 
     
+    
+        
